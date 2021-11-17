53d0a961a8f8bffd4d01af993fef684df6881568
Valentin Cluzeau <vltclz1@gmail.com>
2021-11-17 02:02:21 +0100

    first seo improvements

--- nextjs/components/Header.jsx
+++ nextjs/components/Header.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { cx } from '@emotion/css';
 import * as styles from './Header.styles';
 
 const pages = [
   {
-    route: process.env.ROUTE_ACCUEIL,
+    route: '/',
     text: 'ACCUEIL',
+    title: 'Camille Desmaison Fernandez',
   },
   {
-    route: process.env.ROUTE_REPORTAGES,
+    route: '/reportages',
     text: 'REPORTAGES',
+    title: 'Reportages',
   },
   {
-    route: process.env.ROUTE_PHOTOS,
+    route: '/photos',
     text: 'PHOTOS',
+    title: 'Photos',
   },
   {
-    route: process.env.ROUTE_CONTACT,
+    route: '/contact',
     text: 'CONTACT',
+    title: 'Contact',
   },
 ];
 
@@ -39,46 +44,52 @@ const socials = [
 
 export default function Header() {
   const router = useRouter();
+  const currentPage = pages.find(({ route }) => route === router.route);
 
   return (
-    <header className={styles.container}>
-      <div className={styles.logoContainer}>
-        <ul className={styles.socials}>
-          {socials.map(({ key, src, href }) => (
-            <li key={key}>
-              <a href={href} target="_blank" rel="noreferrer">
-                <Image src={src} width={16} height={16} priority />
-              </a>
-            </li>
-          ))}
-        </ul>
+    <>
+      <Head>
+        <title>{currentPage.title}</title>
+      </Head>
+      <header className={styles.container}>
+        <div className={styles.logoContainer}>
+          <ul className={styles.socials}>
+            {socials.map(({ key, src, href }) => (
+              <li key={key}>
+                <a href={href} target="_blank" rel="noreferrer">
+                  <Image src={src} width={16} height={16} priority />
+                </a>
+              </li>
+            ))}
+          </ul>
 
-        <Link href="/">
-          <span className={styles.nameLogo}>
-            CAMILLE
-            <br />
-            DESMAISON
-            <br />
-            FERNANDEZ
-          </span>
-        </Link>
-      </div>
+          <Link href="/">
+            <span className={styles.nameLogo}>
+              CAMILLE
+              <br />
+              DESMAISON
+              <br />
+              FERNANDEZ
+            </span>
+          </Link>
+        </div>
 
-      <nav>
-        <ul className={styles.navList}>
-          {pages.map(({ route, text }) => (
-            <li
-              key={`navLink-${text}`}
-              className={cx([
-                styles.navElement,
-                router.asPath === route && styles.activeNavElement,
-              ])}
-            >
-              <Link href={route}>{text}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+        <nav>
+          <ul className={styles.navList}>
+            {pages.map(({ route, text }) => (
+              <li
+                key={`navLink-${text}`}
+                className={cx([
+                  styles.navElement,
+                  router.route === route && styles.activeNavElement,
+                ])}
+              >
+                <Link href={route}>{text}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -3,10 +3,6 @@ module.exports = {
   env: {
     STRAPI_URL: process.env.STRAPI_URL,
     IMAGES_DOMAIN: process.env.IMAGES_DOMAIN,
-    ROUTE_ACCUEIL: process.env.ROUTE_ACCUEIL,
-    ROUTE_REPORTAGES: process.env.ROUTE_REPORTAGES,
-    ROUTE_PHOTOS: process.env.ROUTE_PHOTOS,
-    ROUTE_CONTACT: process.env.ROUTE_CONTACT,
     ENDPOINT_ACCUEIL: process.env.ENDPOINT_ACCUEIL,
     ENDPOINT_REPORTAGES: process.env.ENDPOINT_REPORTAGES,
     ENDPOINT_PHOTOS: process.env.ENDPOINT_PHOTOS,
