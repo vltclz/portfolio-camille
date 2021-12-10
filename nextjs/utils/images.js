2aa80ba750833f72a2d5006dfba00a01945382e8
Valentin Cluzeau <vltclz1@gmail.com>
2021-12-10 19:14:26 +0100

    new files

---
+++ nextjs/utils/images.js
@@ -0,0 +1,14 @@
+const formats = ['large', 'medium', 'small', 'thumbnail'];
+
+// eslint-disable-next-line import/prefer-default-export
+export function photoUrl(photoObject, maxFormat) {
+  if (!maxFormat) {
+    return `${process.env.STRAPI_URL}${photoObject.url}`;
+  }
+  const possibleFormats = formats.slice(formats.indexOf(maxFormat));
+  const url = possibleFormats.reduce(
+    (prev, curr) => prev ?? photoObject.formats[curr]?.url,
+    null
+  );
+  return `${process.env.STRAPI_URL}${url}`;
+}
